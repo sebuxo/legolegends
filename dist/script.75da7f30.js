e2dd0e45033c14f2f5f413c920c31bbd1b0661c5
sebuxo <hell.daoum@gmail.com>
2021-07-22 20:20:36 +0100

    test

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -11402,7 +11402,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50986" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63349" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
